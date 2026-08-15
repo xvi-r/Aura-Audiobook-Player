c7b9e10d547880b2b4537fcdbc53b0735fe45d56
xvi-r <luismoreno7117@gmail.com>
2026-08-15 12:38:51 +0200

    Create AudiobookProgress Entity

--- src/main/java/com/example/audiobooks/entity/Audiobook.java
+++ src/main/java/com/example/audiobooks/entity/Audiobook.java
@@ -11,7 +11,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
---
+++ src/main/java/com/example/audiobooks/entity/AudiobookProgress.java
@@ -0,0 +1,36 @@
+package com.example.audiobooks.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class AudiobookProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;    
+
+    //Each AudiobookProgress entity relates to exactly 1 audibook, we don't want multiple progress markers for 1 audiobook
+    @OneToOne()
+    @JoinColumn(name = "audiobook_id", nullable = false, unique = true)
+    private Audiobook audiobook;
+
+    private Double position;
+
+    private boolean completed;
+     
+}
