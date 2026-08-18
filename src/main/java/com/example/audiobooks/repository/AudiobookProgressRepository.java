a6fda1406a4beb99ae06c1dfe9cc615d750e3430
xvi-r <luismoreno7117@gmail.com>
2026-08-18 13:53:43 +0200

    add user repository and annotate repositories

--- src/main/java/com/example/audiobooks/repository/AudiobookProgressRepository.java
+++ src/main/java/com/example/audiobooks/repository/AudiobookProgressRepository.java
@@ -5,7 +5,9 @@ import com.example.audiobooks.entity.AudiobookProgress;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AudiobookProgressRepository extends JpaRepository<AudiobookProgress, Long> {
 
     Optional<AudiobookProgress> findByAudiobookId(Long audiobookId);
--- src/main/java/com/example/audiobooks/repository/EBookRepository.java
+++ src/main/java/com/example/audiobooks/repository/EBookRepository.java
@@ -3,9 +3,11 @@ package com.example.audiobooks.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.audiobooks.entity.EBook;
 
+@Repository
 public interface EBookRepository extends JpaRepository<EBook, Long> {
 
     Optional<EBook> findByAudioBookId(Long audioBookId);
