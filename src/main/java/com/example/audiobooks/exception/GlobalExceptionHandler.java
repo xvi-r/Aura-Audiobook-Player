a0ea2f62b7de26c3c50440e9cf9331776086a7da
xvi-r <luismoreno7117@gmail.com>
2026-08-20 00:25:34 +0200

    add global handler for duplicate usernames

---
+++ src/main/java/com/example/audiobooks/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.audiobooks.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNameAlreadyExistsException.class)
+    public ResponseEntity<String> handleUsernameAlreadyExists(
+            UserNameAlreadyExistsException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(ex.getMessage());
+    }
+}
