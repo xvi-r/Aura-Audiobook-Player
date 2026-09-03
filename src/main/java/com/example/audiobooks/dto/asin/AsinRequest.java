61af357077741f7b99a29543131e438f48c485f6
xvi-r <luismoreno7117@gmail.com>
2026-09-03 22:51:36 +0200

    feat(dto): add fetchChapters to AsinRequest record

--- src/main/java/com/example/audiobooks/dto/asin/AsinRequest.java
+++ src/main/java/com/example/audiobooks/dto/asin/AsinRequest.java
@@ -3,7 +3,6 @@ package com.example.audiobooks.dto.asin;
 import lombok.Getter;
 import lombok.Setter;
 
+public record AsinRequest(String asin, String country, Boolean fetchChapters) {
 
-public record AsinRequest(String asin, String country) {
-    
 }
